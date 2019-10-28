d741025076b58552b4a39284bf7640b30c3db2d3
bHo321 <37441816+bHo321@users.noreply.github.com>
2019-10-28 21:58:40 +0530

    Update multiplication.c

--- multiplication.c
+++ multiplication.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
+
 int main()
 {
-  printf("Enter Your For Multiplication => ");
+  printf("Multiplication of 2 Number :\n");
   float a,b,mul;
   printf("Enter Your First Number => ");
   scanf("%f",&a);
   printf("Enter Your Second Number => ");
   scanf("%f",&b);
   mul=a*b;
-  printf("you multiplication is => ");
-  printf("%f",mul)
+  printf("Your Multiplication Result => ");
+  printf("%0.2f",mul);
+  return 0;
 }
